55d680ca33cc1a62796c46073de736b1b6591149
Roberto Alvarez <ingcram@MacBook-Pro-de-Roberto.local>
2017-09-20 19:12:38 -0500

    se agregan imágenes y se liga los campos del cell

---
+++ MiPrimeraTabla/cellMainTable.h
@@ -0,0 +1,16 @@
+//
+//  cellMainTable.h
+//  MiPrimeraTabla
+//
+//  Created by Roberto Alvarez on 20/09/17.
+//  Copyright © 2017 Roberto Alvarez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface cellMainTable : UITableViewCell
+@property (weak, nonatomic) IBOutlet UIImageView *imgHero;
+@property (weak, nonatomic) IBOutlet UILabel *nameHero;
+@property (weak, nonatomic) IBOutlet UILabel *ageHero;
+
+@end
